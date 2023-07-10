895511211ce40373fe93cd9e06bb9d28f817d31e
PashkovDanilASU <90184602+PashkovDanilASU@users.noreply.github.com>
2023-07-10 20:02:06 +0300

    Добавил requirements.txt. Считывание данных парсером с сайта (для каждого патента), вывод производится print'ом.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Всякие предварительные настройки типо драйвер подключить для браузера(у нас хром, пришлось скачать сука)
+# Всякие предварительные настройки типо драйвер подключить для браузера(у нас хром)
 service = Service(executable_path='./chromedriver.exe')
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
@@ -18,6 +18,7 @@ driver.maximize_window()
 html = driver.page_source
 
 try:
+    count_parcing = 0  # добавил счетчик для подсчета количества полей обработанных парсером
     driver.get(url)
     time.sleep(5)
     input_field = driver.find_element(By.ID, "simple_search_input").send_keys("искусственный интеллект")
@@ -49,7 +50,21 @@ try:
             break
         else:
             # Делаем дела парсим шмарсим на конкретной страничке - ТЕБЕ СЮДА ЧЕТО ДОБАВИТЬ
-            wait = WebDriverWait(driver, 10)
+            table_ = driver.find_element(By.CLASS_NAME,
+                                         "table_default")  # Класс table_default, где располагаются поля таблицы. Класс table_default находится в классе "content_flex content_flex_outer".
+
+            informations = table_.find_elements(By.TAG_NAME,
+                                                "td")  # Считывает все тэги в таблице (должно быть 700 (т.к. 7 столбцов у каждого патента, а отображается 100 патентов), выходит 700)
+
+            for information in informations:  # Считываем информацию из тэгов (то есть данные самого патента, в данном цикле считываются 100 патентов подряд)
+                count_new_line = +1
+                patent_data = information.text
+                print(patent_data)
+                count_parcing += 1
+                if count_parcing % 7 == 0:
+                    print()
+
+            wait = WebDriverWait(driver, 1)
             find_next_element = wait.until(EC.element_to_be_clickable(
                 (By.XPATH,
                  "/html/body/div/div/div[3]/div/div[4]/div[4]/div/div[3]/ul/li[9]/a")))  # полный путь до кнопки > отвечающей за переход на следующую страницу + ожидание кликабельности(чтоб сайт успел прогрузить и не выдал ошибку
@@ -64,9 +79,6 @@ except Exception as _ex:
     print(_ex)
 
 finally:
+    print(count_parcing)
     driver.close()
     driver.quit()
-
-# result = requests.get(url)
-# doc = BeautifulSoup(result.text,"html.parser")
-# print(doc)
