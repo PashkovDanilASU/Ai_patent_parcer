4445a0c7ad747bf156d72113af5d54a35a297da8
oliver2214 <dima.tovstokor@yandex.ru>
2023-07-15 18:17:42 +0500

    Законченный парсинг сайта, патенты выводятся в консоль в виде словарей, парсинг логируется

--- wipo_parsing.py
+++ wipo_parsing.py
@@ -4,64 +4,178 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-import time
+import time, datetime
 import os
 
 
-def changeSelectOption(searching_by, searching_string, position):
-    # Поиск
-    button = driver.find_element(searching_by, searching_string)
-    # Создаем объект Select для работы с элементом <select>
-    select = Select(button)
-    # Выбираем опцию
-    select.select_by_value(position)
-    time.sleep(5)
+def main():
+    def changeSelectOption(searching_by, searching_string, position, sec):
+        '''
+        Изменение выбранного параметра выбранного элемента
+
+        :param searching_by: Метод, используемый для поиска элемента. Допустимые значения: 'id', 'class_name', 'name', 'tag_name', 'css_selector' или 'xpath'.
+        :param searching_string: Значение, используемое для поиска элемента на основе указанного метода поиска.
+        :param position: Значение опции, которую нужно выбрать.
+        :param sec: Время ожидания после установки поля
+        :return: None
+        '''
+        # Поиск
+        button = driver.find_element(searching_by, searching_string)
+        # Создаем объект Select для работы с элементом <select>
+        select = Select(button)
+        # Выбираем опцию
+        select.select_by_value(position)
+        time.sleep(sec)
+
+    def validateQuery(query):
+        '''
+        Функция для валидации данных при парсинге сайта для каждого поля патента
+        Чтобы не было лишних символов, приходил None, вместо "\n" и т.д.
+
+        :param query: Наш запрос к html коду патента
+        :return: query_result
+        '''
+
+        query_result = query
+        if not query_result:
+            return None
+        query_result = query_result.text.strip()
+        if not query_result:
+            return None
+        if query_result.isdigit():
+            return int(query_result)
+        return query_result
+
+    def parseEveryPage():
+        '''
+        Основная функция парсинга.
+
+        :return: Словарь log, содержащий информацию о парсинге.
+        '''
+
+        def parsePage():
+            '''
+            Парсит каждую страницу и извлекает информацию о патентах.
+
+            :return: None
+            '''
+
+            # Получаем HTML-код страницы с помощью Selenium
+            html = driver.page_source
+            # Создаем объект BeautifulSoup для парсинга HTML-кода
+            soup = BeautifulSoup(html, "html.parser")
+
+            # В цикле создается словарь данных для каждого патента
+            for el in soup.find_all(class_="ps-patent-result"):
+                patent = dict()
+                patent_title = validateQuery(el.find("span", class_="needTranslation-title"))
+                patent["patent_title"] = patent_title.capitalize() if patent_title else None
+                patent["publication_date"] = validateQuery(el.select_one('span[id$="resultListTableColumnPubDate"]'))
+                patent["document_number"] = validateQuery(
+                    el.find("span", class_="notranslate ps-patent-result--title--patent-number"))
+                patent["patent_owner"] = validateQuery(
+                    el.find("span", class_="ps-field--value ps-patent-result--applicant notranslate"))
+                patent["applicant"] = validateQuery(
+                    el.find("span", class_="ps-field--value ps-patent-result--inventor notranslate"))
+                patent["international_patent_classification"] = validateQuery(el.select_one('span[id$="PCTipc"]'))
+                patent["country"] = validateQuery(
+                    el.find("div", class_="ps-patent-result--title--ctr-pubdate").select_one("span"))
+
+                # Если отсутствует хотя бы одно обязательное поле, патент в базу не идет
+                if not patent.get("patent_title") or not patent.get("publication_date") or \
+                        not patent.get("international_patent_classification"):
+                    log['missed_patents'] += 1
+                    continue
+
+                # Вместо этого принта будет запись патента в базу данных
+                print(patent)
+                log["parsed_patents"] += 1
+
+        def nextPage():
+            '''
+            Переходит на следующую страницу и возвращает статус перехода.
+
+            :return: True, если переход выполнен успешно. False, если больше нет следующей страницы.
+            '''
+            next_button = False
+            # Попытка найти неактивную кнопку на следующую страницу
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, ".js-paginator-next.ui-state-disabled")
+            except:
+                pass
+            # Если таковую нашли, значит данная страница последняя
+            if next_button:
+                return False
+            # Если это не так, то ищем активную кнопку на следующую страницу и жмем её
+            next_button = driver.find_element(By.CLASS_NAME, "js-paginator-next")
+            next_button.click()
+            time.sleep(1)
+            return True
+
+        # Логирование парсера, в log будет содержаться информация о парсинге сайта
+        log = {
+            'parsed_patents': 0,
+            'missed_patents': 0,
+            'status': 'Parsing in progress',
+            'timestamp': None
+        }
+        try:
+            while True:
+                parsePage()
+                if not nextPage():
+                    log['status'] = 'Parsing completed'
+                    log['timestamp'] = datetime.datetime.utcnow()
+                    return log
+
+        except Exception as e:
+            log['status'] = 'Parsing error: {}'.format(str(e))
+            log['timestamp'] = datetime.datetime.utcnow()
+            return log
+
+    def configureSearch(search_term):
+        '''
+        Выполняет предварительную настройку сайта
+
+        :param search_term: Поисковой запрос, по которому будет парсится сайт
+        :return: None
+        '''
+
+        # Запуск браузера и открытие веб-страницы
+        driver.maximize_window()
+        driver.get(url)
+        time.sleep(5)
+
+        # настройка поиска на сайте по любому полю, а не только в заголовке
+        changeSelectOption(By.ID, "simpleSearchForm:field:input", "RU_ALL", 1)
+        # Ищем поле ввода поиска на сайте
+        search_form = driver.find_element(By.ID, "simpleSearchForm:fpSearch:input")
+        # В поле ввода пишем Artificial intelligence и жмем поиск
+        search_form.clear()
+        search_form.send_keys(search_term)
+        search_form.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        changeSelectOption(By.ID, "resultListCommandsForm:viewType:input", "DOUBLE_VIEW", 3)
+        changeSelectOption(By.ID, "resultListCommandsForm:perPage:input", "200", 5)
+
+    # Домен сайта с патентами
+    url = "https://patentscope.wipo.int/search/ru/result.jsf?_vid=P20-LK2O66-06230"
+
+    # Предварительные настройки: драйвер подключить для браузера(хром)
+    service = Service(executable_path='./chromedriver.exe')
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+
+    try:
+        configureSearch("Искусственный интеллект")
+        print(parseEveryPage())
+
+    except Exception as ex:
+        print(ex)
+    finally:
+        driver.close()
+        driver.quit()
 
 
-url = "https://patentscope.wipo.int/search/ru/result.jsf?_vid=P20-LK2O66-06230"
-
-# Всякие предварительные настройки типо драйвер подключить для браузера(у нас хром)
-service = Service(executable_path='./chromedriver.exe')
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
-
-try:
-    # Запуск браузера и открытие веб-страницы
-    driver.maximize_window()
-    driver.get(url)
-    time.sleep(5)
-
-    # В поле ввода пишем Artificial intelligence и жмем поиск
-    search_form = driver.find_element(By.ID, "simpleSearchForm:fpSearch:input")
-    search_form.clear()
-    search_form.send_keys("Artificial intelligence")
-    search_form.send_keys(Keys.ENTER)
-    time.sleep(3)
-
-    changeSelectOption(By.ID, "resultListCommandsForm:viewType:input", "DOUBLE_VIEW")
-    changeSelectOption(By.ID, "resultListCommandsForm:perPage:input", "200")
-
-    # Получаем HTML-код страницы с помощью Selenium
-    html = driver.page_source
-
-    # Создаем объект BeautifulSoup для парсинга HTML-кода
-    soup = BeautifulSoup(html, "html.parser")
-
-    # В цикле создается словарь данных для каждого патента
-    for el in soup.find_all(class_="ps-patent-result"):
-        patent = dict()
-        patent["patent_title"] = el.find("span", class_="needTranslation-title").text
-        patent["publication_date"] = el.select_one('span[id$="resultListTableColumnPubDate"]').text.strip()
-        patent["document_number"] = el.find("span",
-                                            class_="notranslate ps-patent-result--title--patent-number").text
-        patent["patent_owner"] = el.find("span", class_="ps-field--value ps-patent-result--applicant notranslate").text
-        patent["applicant"] = el.find("span", class_="ps-field--value ps-patent-result--inventor notranslate").text
-        patent["international_patent_classification"] = el.select_one('span[id$="PCTipc"]').text.strip()
-        patent["country"] = el.find("div", class_="ps-patent-result--title--ctr-pubdate").select_one("span").text.strip()
-        print(patent)
-
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+if __name__ == "__main__":
+    main()
