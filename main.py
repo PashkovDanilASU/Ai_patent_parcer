249336f8ccb81c1b26ae93548e3ccb1a74e49086
PashkovDanilASU <90184602+PashkovDanilASU@users.noreply.github.com>
2023-07-17 22:27:03 +0300

    Добавлено сохранение патентов в json файл, список состоящий из словарей, фильтрация патентов по "нормальным", чтение из json файла в словарь.

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from datetime import date
 import time
+import json
 
 # Всякие предварительные настройки типо драйвер подключить для браузера(у нас хром)
 service = Service(executable_path='./chromedriver.exe')
@@ -20,6 +22,9 @@ html = driver.page_source
 
 try:
     count_parcing = 0  # добавил счетчик для подсчета количества полей обработанных парсером
+    zero_date = date(2000,1,1) # нулевая дата для добавления в "конструктор" патента
+    all_patents_list = []  # Создаю пустой список, где будут находится все патенты (обращение к элементам списка по индексам начиная с 0)
+    all_patents_list_true =[]
     driver.get(url)
     time.sleep(3)
     input_field = driver.find_element(By.ID, "simple_search_input").send_keys("искусственный интеллект")
@@ -49,9 +54,7 @@ try:
     page_scrolling = (amount_patents + el_per_page - 1) // el_per_page
     button.click()
     time.sleep(1)
-
-    for i in range(page_scrolling):  # Не попадает в условие if - помочь братку
-            # Делаем дела парсим шмарсим на конкретной страничке - ТЕБЕ СЮДА ЧЕТО ДОБАВИТЬ
+    for i in range(page_scrolling):
         table_ = driver.find_element(By.CLASS_NAME,
                                          "table_default")  # Класс table_default, где располагаются поля таблицы. Класс table_default находится в классе "content_flex content_flex_outer".
 
@@ -59,13 +62,67 @@ try:
                                                 "td")  # Считывает все тэги в таблице (должно быть 700 (т.к. 7 столбцов у каждого патента, а отображается 100 патентов), выходит 700)
 
         for information in informations:  # Считываем информацию из тэгов (то есть данные самого патента, в данном цикле считываются 100 патентов подряд)
-            count_new_line = +1
-            patent_data = information.text
-            print(patent_data)
-            count_parcing += 1
             if count_parcing % 7 == 0:
-                print()
+                patent = {'patent_number': '_', 'patent_publication_date': zero_date, 'patent_name': '_', 'applicant': '_',
+                           'author': '_',
+                           'copyright_holder': '_', 'mpc_index': '_', 'spk_index': '_'} # конструктор патента, который не содержит особой информации
+                all_patents_list.append(patent)
+            patent_data = information.text
+            if count_parcing % 7 == 0: #Здесь разделяется информация про номер патента и дата его публикации
+                if '0' in patent_data[-5] and '0' in patent_data[-2]:
+                    patent_data_date = date(int(patent_data[-10:-6]), int(patent_data[-4]), int(patent_data[-1]))
+                elif '0' in patent_data[-5] and '0' not in patent_data[-2]:
+                    patent_data_date = date(int(patent_data[-10:-6]), int(patent_data[-4]), int(patent_data[-2:]))
+                elif '0' not in patent_data[-5] and '0' in patent_data[-2]:
+                    patent_data_date = date(int(patent_data[-10:-6]), int(patent_data[-5:-3]), int(patent_data[-1]))
+                else:
+                    patent_data_date = date(int(patent_data[-10:-6]), int(patent_data[-5:-3]), int(patent_data[-2:]))
+                patent_data = patent_data[:-10].strip()
+                patent['patent_number'] = patent_data
+                patent['patent_publication_date'] = patent_data_date.isoformat()
+            elif count_parcing % 7 == 1: # Далее идет каскад условий, какому ключу словаря (патента) присвоить его значение поля
+                patent['patent_name'] = patent_data
+            elif count_parcing % 7 == 2:
+                patent['applicant'] = patent_data
+            elif count_parcing % 7 == 3:
+                patent['author'] = patent_data
+            elif count_parcing % 7 == 4:
+                patent['copyright_holder'] = patent_data
+            elif count_parcing % 7 == 5:
+                patent['mpc_index'] = patent_data
+            else:
+                patent['spk_index'] = patent_data
+            count_parcing += 1
         time.sleep(1)
+        if i == 9: # здесь сохраняем акутальные патенты в json файл
+            for patent_ in all_patents_list:
+                if patent_['applicant'] == '—' or patent_['author'] == '—' or patent_['copyright_holder'] == '—':
+                    continue
+                else:
+                    all_patents_list_true.append(patent_)
+            del all_patents_list #удаляю старый список, для очистки памяти
+            with open("all_patents_list_true.json", "w", encoding="utf-8") as file:
+                json.dump(all_patents_list_true, file, indent=4, ensure_ascii=False)
+            #print(all_patents_list_true)
+             #здесь добавлено чтени из json файла, чтобы перезаписать словарь (в котором будут находиться акутальные патенты)
+            with open("all_patents_list_true.json", "r",  encoding="utf-8") as file:
+                data = json.load(file)
+                for obj in data:
+                    patent = {}
+                    patent['patent_number'] = obj['patent_number']
+                    #
+                    date_string = obj['patent_publication_date']
+                    date_python = date.fromisoformat(date_string)
+                    #
+                    patent['patent_publication_date'] = date_python
+                    patent['patent_name'] = obj['patent_name']
+                    patent['applicant'] = obj['applicant']
+                    patent['author'] = obj['author']
+                    patent['copyright_holder'] = obj['copyright_holder']
+                    patent['mpc_index'] = obj['mpc_index']
+                    patent['spk_index'] = obj['spk_index']
+                    all_patents_list_true.append(patent)
+            #print(all_patents_list_true)
         footer = driver.find_element(By.CLASS_NAME, "pagNextPage_ru")
         delta_y = footer.rect['y']
         ActionChains(driver) \
@@ -84,7 +141,6 @@ try:
         find_next_element = driver.find_element(By.CLASS_NAME, "pagNextPage_ru") # Ищем кнопку перехода на некст страницу патентов
         find_next_element.click()
         time.sleep(1)
-
 
     time.sleep(10)
 except Exception as _ex:
